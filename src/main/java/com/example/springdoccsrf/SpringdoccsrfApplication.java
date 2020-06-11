95285724de68f05b27c45c0bf3b252aa8dc35b49
Andreas Stjerndal <stjernd@kth.se>
2020-06-11 10:13:35 +0200

    Second

--- src/main/java/com/example/springdoccsrf/SpringdoccsrfApplication.java
+++ src/main/java/com/example/springdoccsrf/SpringdoccsrfApplication.java
@@ -2,21 +2,12 @@ package com.example.springdoccsrf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringdoccsrfApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringdoccsrfApplication.class, args);
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        return new CorsFilter(source);
     }
 
 }
